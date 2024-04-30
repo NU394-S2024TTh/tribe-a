e396e9638f299e7a7cdb76f399997a1b5621853a
Todd Warren <todd@warrenfamily.org>
2024-04-30 07:42:14 -0600

    add readme, cleanup eslint for functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -21,13 +21,12 @@ module.exports = {
     "/lib/**/*", // Ignore built files.
     "/generated/**/*", // Ignore generated files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
-    "quotes": ["error", "double"],
+    "quote-props": ["error", "as-needed"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    indent: ["error", 2],
+    "linebreak-style": "off",
   },
 };
