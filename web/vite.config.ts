83d0ac154561ace9ff29f6fea2244a4b805bd8b5
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-05-09 06:59:41 -0500

    fix the error in ci/cd for rollup

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,4 +6,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	test: { globals: true, environment: 'jsdom' },
 	plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@langchain/core']
+    }
+  },
 });
