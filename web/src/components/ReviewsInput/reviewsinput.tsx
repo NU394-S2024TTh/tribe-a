bcf48ee3b44e2b073dc65a3fdb7bec2a2fb0aece
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-05-09 06:09:39 -0500

    Integrate Sentiment Analyzer

--- web/src/components/ReviewsInput/reviewsinput.tsx
+++ web/src/components/ReviewsInput/reviewsinput.tsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import sentimentAnalyzer from '../../processes/sentimentAnalyzer.mjs';
 
 export default function ReviewsInput() {
     const [input, setInput] = useState<string>('');
     const [reviews, setReviews] = useState<string[]>([]); 
+
+    // run sentiment analysis on reviews state change
+    async function handleReviewsChange (reviews: string[]) {
+        console.log("Reviews state changed: ", reviews);
+        const sentiments = await sentimentAnalyzer.getSentiments(reviews); // run sentiment analysis on reviews
+        
+        for (let i = 0; i < reviews.length; i++) {
+            alert(`Review: ${reviews[i]}\nSentiment: ${sentiments[i]}`); // alert sentiment analysis results
+        }
+        
+        return sentiments;
+    }
+
+    useEffect(() => {handleReviewsChange(reviews)}, [reviews]);
 
     const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setInput(event.target.value);
