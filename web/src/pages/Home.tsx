c10adc76db6633acead3204a02e4c3f183110cba
elliasc04 <guoyethan@gmail.com>
2024-05-09 09:41:53 -0500

    homepage refactor

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -4,9 +4,11 @@ import ReviewsInput from '../components/ReviewsInput/reviewsinput';
 
 export default function Home() {
 	return (
-		<div className="flex h-screen flex-col items-center justify-center bg-[#132a3a]">
-			<div className="mt-20">
+		<div className="flex h-screen flex-row items-center justify-center bg-[#132a3a]">
+			<div className="mt-10 mr-20">
 				<ReviewsInput />
+			</div>
+			<div className="mt-10">
 				<Tabs data={TEST_DATA}></Tabs>
 			</div>
 		</div>
