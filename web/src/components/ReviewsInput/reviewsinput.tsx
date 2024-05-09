3fa09d85fdfc5c52fc48e440dd37fbe5a9bc8d1c
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-05-09 06:17:07 -0500

    linting

--- web/src/components/ReviewsInput/reviewsinput.tsx
+++ web/src/components/ReviewsInput/reviewsinput.tsx
@@ -1,48 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import sentimentAnalyzer from '../../processes/sentimentAnalyzer.mjs';
 
 export default function ReviewsInput() {
-    const [input, setInput] = useState<string>('');
-    const [reviews, setReviews] = useState<string[]>([]); 
+	const [input, setInput] = useState<string>('');
+	const [reviews, setReviews] = useState<string[]>([]);
 
-    // run sentiment analysis on reviews state change
-    async function handleReviewsChange (reviews: string[]) {
-        console.log("Reviews state changed: ", reviews);
-        const sentiments = await sentimentAnalyzer.getSentiments(reviews); // run sentiment analysis on reviews
-        
-        for (let i = 0; i < reviews.length; i++) {
-            alert(`Review: ${reviews[i]}\nSentiment: ${sentiments[i]}`); // alert sentiment analysis results
-        }
-        
-        return sentiments;
-    }
+	// run sentiment analysis on reviews state change
+	async function handleReviewsChange(reviews: string[]) {
+		console.log('Reviews state changed: ', reviews);
+		const sentiments = await sentimentAnalyzer.getSentiments(reviews); // run sentiment analysis on reviews
 
-    useEffect(() => {handleReviewsChange(reviews)}, [reviews]);
+		for (let i = 0; i < reviews.length; i++) {
+			alert(`Review: ${reviews[i]}\nSentiment: ${sentiments[i]}`); // alert sentiment analysis results
+		}
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setInput(event.target.value);
-    };
+		return sentiments;
+	}
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // splits reviews by new line  
-        setReviews(input.trim().split('\n'));
-        // console.log(input.trim().split('\n'));
-    };
-    
-    return (
-        <div className="flex justify-center items-center my-5">
-            <form onSubmit={handleSubmit} className="w-full max-w-sm">
-                <label htmlFor="textInput" className="block mb-2 text-white">Enter reviews:</label>
-                <textarea
-                    id="textInput"
-                    value={input}
-                    onChange={handleInputChange}
-                    className="w-full px-3 py-2 border rounded-md resize-none focus:outline-none focus:border-custom-theme-2"
-                    rows={4}
-                />
-                <button type="submit" className="mt-2 w-full bg-custom-theme-2 text-white px-4 py-2 rounded-md hover:bg-custom-theme-2b focus:outline-none focus:bg-custom-theme-2 border border-white">Submit</button>
-            </form>
-        </div>
-    );
+	useEffect(() => {
+		handleReviewsChange(reviews);
+	}, [reviews]);
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setInput(event.target.value);
+	};
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		// splits reviews by new line
+		setReviews(input.trim().split('\n'));
+		// console.log(input.trim().split('\n'));
+	};
+
+	return (
+		<div className="my-5 flex items-center justify-center">
+			<form onSubmit={handleSubmit} className="w-full max-w-sm">
+				<label htmlFor="textInput" className="mb-2 block text-white">
+					Enter reviews:
+				</label>
+				<textarea
+					id="textInput"
+					value={input}
+					onChange={handleInputChange}
+					className="w-full resize-none rounded-md border px-3 py-2 focus:border-custom-theme-2 focus:outline-none"
+					rows={4}
+				/>
+				<button
+					type="submit"
+					className="mt-2 w-full rounded-md border border-white bg-custom-theme-2 px-4 py-2 text-white hover:bg-custom-theme-2b focus:bg-custom-theme-2 focus:outline-none"
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
