482da028e9233c8b254469b89397a9ef00cc6298
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-05-20 12:28:51 -0500

    add tests for the home screen and the spike

--- web/src/app.test.tsx
+++ web/src/app.test.tsx
@@ -1,18 +1,57 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
 
-import App from './components/spike/App';
+import App from './components/Spike/App';
+import Home from './pages/Home';
 
-describe('counter tests', () => {
-	test('Counter should be 0 at the start', () => {
+// https://github.com/recharts/recharts/issues/2268#issuecomment-1129412873
+// https://jskim1991.medium.com/react-writing-tests-with-graphs-9b7f2c9eeefc
+// https://github.com/recharts/recharts/issues/2982
+// Mock the ResponsiveContainer of recharts module
+// https://vitest.dev/guide/mocking.html#modules
+// vi.mock('recharts', async (importOriginal) => {
+//   const OriginalRechartsModule: any = await importOriginal();
+// 	console.log('OriginalRechartsModule', OriginalRechartsModule);
+
+// 	// Define the mock component
+// 	const MockResponsiveContainer = ({ children }: { children: React.ReactNode }) => (
+//     // <div className="recharts-responsive-container" style={{ width: 800, height: 600 }}>
+//     //   {children}
+//     // </div>
+// 		<OriginalRechartsModule.ResponsiveContainer width={800} height={800}>
+// 			{children}
+// 		</OriginalRechartsModule.ResponsiveContainer>
+//   );
+
+//   // Return the original module with the ResponsiveContainer mocked
+//   return {
+//     ...(OriginalRechartsModule as { [key: string]: any }),  // type assertion to tell TypeScript that OriginalRechartsModule is an object
+//     ResponsiveContainer: MockResponsiveContainer,
+//   };
+// });
+
+describe('Spike tests', () => {
+	test('Tribe A Members should appear', () => {
 		render(<App />);
-		expect(screen.getByText('count is: 0')).toBeDefined();
+		expect(screen.getByText('Tribe A Members')).toBeDefined();
 	});
+});
 
-	test('Counter should increment by one when clicked', async () => {
-		render(<App />);
-		const counter = screen.getByRole('button');
-		fireEvent.click(counter);
-		expect(await screen.getByText('count is: 1')).toBeDefined();
+describe('Home screen tests', () => {
+	test('Sentiment Analysis should appear', () => {
+		render(<Home />);
+		expect(screen.getByText('Sentiment Analysis')).toBeDefined();
 	});
+	test('A line graph with value 0.0 should appear', () => {
+		render(<Home />);
+		expect(screen.getByText('0.0')).toBeDefined();
+	});
+	// test('A graph with text IMDb should appear when Sentiment Analysis is clicked', async () => {
+	// 	render(<Home />);
+	// 	const button = screen.getByText('Sentiment Analysis');
+	// 	await fireEvent.click(button);
+
+	// 	// wait for 3 seconds for the graph to appear manually in the browser
+	// 	await waitFor(() => {expect(screen.getByText('IMDb')).toBeDefined() }, { timeout: 3000 });
+	// });
 });
