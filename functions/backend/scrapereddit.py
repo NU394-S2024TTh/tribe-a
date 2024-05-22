74f8dd42534e2c8c063d26c213aec0d369e266c9
Tina Chen <tinachen2025@u.northwestern.edu>
2024-05-22 02:52:34 -0500

    fetched comments from reddit post

---
+++ functions/backend/scrapereddit.py
@@ -0,0 +1,38 @@
+import praw
+import json
+# Reddit API credentials
+client_id = 'VVDFsXhfzl5Bk_Il_ykTwg'
+client_secret = '1PSjnF_Od4nMEm7AuOyUgl0fNO_PMw'
+user_agent = 'reddituser1039412'
+
+# Initialize Reddit instance
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+
+def scrape_reddit_reviews(show_name, post_url):
+    submission = reddit.submission(url=post_url)
+    
+    # Ensure that all comments are loaded
+    submission.comments.replace_more(limit=None)    
+    reviews = []
+    
+    for review in submission.comments:
+        # Filter comments
+
+        # Format as JSON
+        reviews.append({
+            "review_content": review.body,
+            "time_published": str(review.created_utc),
+            "season": 1,
+            "episode": 1,
+            "user_demographic_data": {
+                "author": review.author.name if review.author else None
+            }
+        })
+    
+    print(reviews)
+    return reviews
+
+# Example usage:
+show_name = "Yellowjackets"
+post_url = "https://www.reddit.com/r/Yellowjackets/comments/qnshoo/episode_discussion_s01e01/"
+scrape_reddit_reviews(show_name, post_url)
