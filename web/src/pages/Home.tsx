9464ce85d768b23f08011647566435481cde6ad4
shawnliang2024 <shawnliang2024@u.northwestern.edu>
2024-05-22 22:26:32 -0500

    frt-13 -- switchable sentiment analysis between shows

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,16 +1,16 @@
 import Tabs from '../components/charts/ChartTabNav';
 import { TEST_DATA } from '../components/charts/testdata';
 import ReviewsInput from '../components/reviews/ReviewsInput';
+import ShowList from './Showlist';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SentimentAnalysis from './Showlist';
+
 
 export default function Home() {
 	return (
-		<div className="flex h-screen flex-row items-center justify-center bg-[#132a3a]">
-			<div className="mr-20 mt-10">
-				<ReviewsInput />
-			</div>
-			<div className="mt-10">
-				<Tabs data={TEST_DATA}></Tabs>
-			</div>
-		</div>
+		<Router>
+      <ShowList streamingservice="Paramount+"/>
+    </Router>
+		
 	);
 }
